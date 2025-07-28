c3ee00baac4516224e1a67b6a05fd683fcb83f51
khizaralamk <khizar@alphaventure.com>
2025-07-28 22:42:25 +0500

    resized the main heading

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -9,7 +9,7 @@ interface HeadingProps {
 export default function Heading({ name, tags }: HeadingProps) {
   return (
     <div className="flex flex-col items-center w-full ">
-      <img src={nameSVG} alt={name} className=" px-4 w-[350px] md:w-[400px] lg:w-[535px] h-auto mx-auto" />
+      <img src={nameSVG} alt={name} className=" px-4 w-[330px] md:w-[400px] lg:w-[535px] h-auto mx-auto" />
       <p className="mt-5 text-xl md:text-3xl text-gray-700 text-center  px-4 py-2">{tags}</p>
     </div>
   );
